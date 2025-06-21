e236384c6934d98112277fcb4b37e8a9ac6ed6a5
Rasmus Pehrsson <pehrsson.rasmus@gmail.com>
2025-06-21 14:04:44 +0200

    little more confidence when searching

--- main.py
+++ main.py
@@ -278,7 +278,7 @@ class GolfBot:
             return
         
         # Filter for confident balls
-        confident_balls = [ball for ball in balls if ball.confidence > 0.2]
+        confident_balls = [ball for ball in balls if ball.confidence > 0.3]
         
         if not confident_balls:
             self.logger.info("No confident ball detections during centering - returning to search")
